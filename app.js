002fed1a5b5065175f40ef00e15c63726120a527
Danie <danicademic@outlook.com>
2024-01-01 22:24:44 +0000

    1º commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// var swiper = new Swiper(".mySwiper", {
+//     direction: "vertical",
+//     slidesPerView: 1,
+//     spaceBetween: 30,
+//     mousewheel: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
+
+  // const skills = document.querySelector(".skills");
+  // skills.style.flex;
+  // skills.style.justifyContent = 'space-around'
