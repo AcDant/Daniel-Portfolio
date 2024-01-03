924ede1f8dfd4ffc3486b67b8dc2b21ea4cb260c
Danie <danicademic@outlook.com>
2024-01-03 19:21:15 +0000

    haciendo cosas con los gradientes en fondo y fonts

--- app.js
+++ app.js
@@ -13,6 +13,15 @@
 // skills.style.flex;
 // skills.style.justifyContent = 'space-around'
 
+let primario = "#ffd319";
+let secundario = "#ff901f";
+let terciario = "#f222ff";
+let cuaternario = "#ff2975";
+let quintinario = "#8c1eff";
+let extra1 = "#5268c1";
+let extra2 = "#7592e5";
+
+let raisinBlack = "#1b1e25ff";
 let verdeOscuro = "#003b00";
 let verde = "#008f11";
 let verdeClaro = "#00ff41";
@@ -23,23 +32,26 @@ const punto2 = document.querySelector("#punto2");
 const punto3 = document.querySelector("#punto3");
 
 const tresPuntos = () => {
+  punto1.style.color = raisinBlack;
+  punto2.style.color = raisinBlack;
+  punto3.style.color = raisinBlack;
   setTimeout(() => {
     // punto1.style.color = "#ffbd00ff";
-    process.style.color = verdeClaro;
-    punto1.style.color = verdeClaro;
+    process.style.color = cuaternario;
+    punto1.style.color = cuaternario;
   }, 1000);
   setTimeout(() => {
     // punto2.style.color = "#ffbd00ff";
-    punto2.style.color = verdeClaro;
+    punto2.style.color = cuaternario;
   }, 2000);
   setTimeout(() => {
     // punto3.style.color = "#ffbd00ff";
-    punto3.style.color = verdeClaro;
+    punto3.style.color = cuaternario;
   }, 3000);
   setTimeout(() => {
-    punto1.style.color = "#1b1e25ff";
-    punto2.style.color = "#1b1e25ff";
-    punto3.style.color = "#1b1e25ff";
+    punto1.style.color = raisinBlack;
+    punto2.style.color = raisinBlack;
+    punto3.style.color = raisinBlack;
   }, 4000);
   setTimeout(() => {
     tresPuntos();
